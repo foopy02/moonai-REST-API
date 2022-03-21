4d51b0ae2383712fd205919d77c08d8a69a340e8
foopy02 <meirabaiuliadil20020@gmail.com>
2022-03-21 23:08:43 +0600

    added JWT authorization

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     # path('user/register', views.register, name='register user'),
     # path('user/login', views.login, name='login user'),
     # path('user/register', views.register, name='register user'),
     # path('user/login', views.login, name='login user'),
-    path('user/<str:uuid_of_user>/withdraws', views.get_withdraws),
-    path('user/<str:uuid_of_user>/deposits', views.get_deposits),
+    path('user/withdraws', views.get_withdraws),
+    path('user/deposits', views.get_deposits),
+    path('api-auth/', include('rest_framework.urls')),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+
 
 ]
+
--- api/views.py
+++ api/views.py
@@ -2,7 +2,9 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Deposit, Withdraw
 from .serializers import WithdrawSerializer, DepositSerializer
 
@@ -15,12 +17,14 @@ def index(request):
 
 #TEST USER ID = ad474b75-2dd6-405f-85ab-e21495dd377e
 @api_view(['GET'])
-def get_withdraws(request, uuid_of_user):
-    withdraws = Withdraw.objects.filter(user_id = uuid_of_user)
-    serializer = WithdrawSerializer(withdraws, many=True)
-    return Response(serializer.data)
+@permission_classes([IsAuthenticated])
+def get_withdraws(request):
+    # headers = request.headers
+    # uuid_of_user = headers.id
+    return JsonResponse({"hello":2})
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_deposits(request, uuid_of_user):
     deposits = Deposit.objects.filter(user_id = uuid_of_user)
     serializer = DepositSerializer(deposits, many=True)

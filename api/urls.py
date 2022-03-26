53aae0df124d91b44046c41054813613ee251468
foopy02 <meirabaiuliadil20020@gmail.com>
2022-03-26 21:24:32 +0600

    update

--- api/urls.py
+++ api/urls.py
@@ -1,23 +1,16 @@
-# from django.urls import path, include
-# from . import views
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-# urlpatterns = [
-#     path('register/', views.register_user),
-#     path('user/', views.get_user_info),
-#     path('user/withdraws/', views.get_withdraws),
-#     path('user/deposits/', views.get_deposits),
-#     # path('user/referal/list', views.get_referals),
-#     # path('user/referal/code', views.get_referal_code),
-#     # path('')
-#     path('api-auth/', include('rest_framework.urls')),
-#     path('token/', TokenObtainPairView.as_view()),
-#     path('token/refresh/', TokenRefreshView.as_view()),
+from django.urls import path, include
+from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+urlpatterns = [
+    path('register/', views.register_user),
+    path('user/', views.get_user_info),
+    path('user/withdraws/', views.get_withdraws),
+    path('user/deposits/', views.get_deposits),
+    # path('user/referal/list', views.get_referals),
+    # path('user/referal/code', views.get_referal_code),
+    # path('')
+    path('api-auth/', include('rest_framework.urls')),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 
-# ]
-
-# from math import factorial
-import random
-
-random.randint(0,5)
-random.randrange(0,66,3)
-random.random()
+]

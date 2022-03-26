3386889ef166e860a85fd4f748d8e02e3dde5c4f
foopy02 <meirabaiuliadil20020@gmail.com>
2022-03-26 21:50:35 +0600

    fix views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from pydantic import Json
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes

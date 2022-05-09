2cd19d3d049829319d79b325f12009fc8b9fde03
foopy02 <meirabaiuliadil20020@gmail.com>
2022-05-09 10:53:22 +0600

    testing celery task

--- moonai_api/celery.py
+++ moonai_api/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moonai_api.settings')
@@ -18,6 +19,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+app.conf.beat_schedule = {
+    'every-5-seconds':{
+        'task': 'api.tasks.test',
+        'schedule': 5,
+    }
+}
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
